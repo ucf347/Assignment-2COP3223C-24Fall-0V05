bee77e581f2e2ca08d3464fa9b752dbc69c2b1ed
ucf347 <blueranger2099@yahoo.com>
2024-09-21 23:21:32 -0400

    Add files via upload

---
+++ continuity.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int main ( void )
+
+int main(int argc, char **argv); 
+
+
+{
+
+	determineMaxItems();
+		float item1 items
+		printf()
+			item1= 2
+			items = item1 ++ 1
+			if ((item % 3 == 0)){
+			item * .66 = coupon66
+			}
+			else {
+			(items = item1 ++ 1)
+			}
+		printf("You can only afford %d items", )
+			
+			
+
+
+{
+
+
+	determineNumericalGrade(char, *gradeCalculation);
+		int g1, g2, g3, g4, g5, g6, g7, g8, firstGrades, secondGrades, thirdGrades, remainingGrades, gradeCalculation, totalGrade;
+		printf("Enter your grades \n");
+			printf("g1 : \n");
+			scanf("%d" , &g1);
+			printf("g2 : \n");
+			scanf("%d" , &g2);
+			printf("g3 : \n");
+			scanf("%d" , &g3);
+			printf("g4 : \n");
+			scanf("%d" , &g4);
+			printf("g5 : \n");
+			scanf("%d" , &g5);
+			printf("g6 : \n");
+			scanf("%d" , &g6);
+			printf("g7 : \n");
+			scanf("%d" , &g7);
+			printf("g8 : \n");
+			scanf("%d" , &g8);
+			printf("\n");
+			
+			
+			firstGrades = (((g1%100) + (g2%100)+(g3%100))%3)*10;
+			secondGrades = (((g4%100) + (g5%100))%2)*20;
+			thirdGrades = ((g6%100)+(g7%100))%2)*5;
+			remainingGrades = ((g8)%100)*65;
+			totalGrade = firstGrades + secondGrades + thirdGrades + remainingGrades;
+
+			outputProcessGrade();
+			{
+			 
+			 if(totalGrade >= 90)
+			{
+				printf("Grade A");
+			}
+			else if(totalGrade>= 80)
+			{
+				printf("Grade B");
+			}
+			else if(totalGrade >= 70)
+			{
+				printf("Grade C");
+			}
+			else if(totalGrade >= 60)
+			{
+				printf("Grade D");
+			}
+			else if(totalGrade >= 50)
+			{
+				printf("Grade F");
+			}
+			printf("The weighted average is: %ls \n", &gradeCalculation);
+			printf ("Your grade based on the average is a: %c \n",  *&letterGrade);
+
+			}
+	
+
+
+
+return 0;
+
+}
+
+
+	sendEmails();
+	 
+		double daysLeft, hoursLeft, timeLeft
+		
+			printf("Enter the days and hours: \n" )
+			scanf(%d , daysLeft)
+			scanf(%d, hoursLeft)
+			daysLeft = 24 
+			if {(timeLeft < 24)
+			else 
+			
+			printf( "%d , \t %d" daysLeft , hoursLeft)
+			print("You have %d hours left \n", &timeLeft
+	 
+	 
+		
+		SendMail(0);
+
+
+
+
+return 0;
+
+}
+gcc /workspaces/official-assignment-2-UCF-Coder347/continuity.c && ./a.out
